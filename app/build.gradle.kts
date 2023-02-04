49fab2428b4428a85b778e039c12436917883aa1
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-04 21:14:25 +0100

    feat: Read app version from version.txt file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0"
+        versionName = rootProject.file("version.txt").readText().trim()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
