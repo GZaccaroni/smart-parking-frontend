5837efbb0631b9e6a66db181ac58423b70a763c1
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-12 13:43:29 +0100

    fix: Register presentation module DI

--- app/src/main/java/it/unibo/smart_parking/MainApplication.kt
+++ app/src/main/java/it/unibo/smart_parking/MainApplication.kt
@@ -3,14 +3,15 @@ package it.unibo.smart_parking
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
-import it.unibo.smart_parking.data.di.dataModule
 import it.unibo.smart_parking.domain.di.domainModule
+import it.unibo.smart_parking.data.di.dataModule
+import it.unibo.smart_parking.presentation.di.presentationModule
 
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            modules(dataModule + domainModule)
+            modules(domainModule + dataModule + presentationModule)
             androidContext(this@MainApplication)
         }
     }
