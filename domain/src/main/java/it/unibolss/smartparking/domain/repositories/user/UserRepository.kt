8da6f38e5118f878612943fae468868b0fa06b4f
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-12 20:33:41 +0100

    feat: Added user repository

---
+++ domain/src/main/java/it/unibolss/smartparking/domain/repositories/user/UserRepository.kt
@@ -0,0 +1,21 @@
+package it.unibolss.smartparking.domain.repositories.user
+
+import arrow.core.Either
+import it.unibolss.smartparking.domain.entities.common.AppError
+import it.unibolss.smartparking.domain.entities.user.AuthState
+import it.unibolss.smartparking.domain.entities.user.User
+import it.unibolss.smartparking.domain.entities.user.UserCredentials
+import kotlinx.coroutines.flow.Flow
+
+interface UserRepository {
+    val authState: AuthState
+    val authStateFlow: Flow<AuthState>
+
+    suspend fun signUp(credentials: UserCredentials): Either<AppError, Unit>
+    suspend fun login(credentials: UserCredentials): Either<AppError, Unit>
+    suspend fun logout(): Either<AppError, Unit>
+    suspend fun changePassword(currentPassword: String): Either<AppError, Unit>
+
+    suspend fun getCurrentUser(): Either<AppError, User>
+    suspend fun deleteCurrentUser(): Either<AppError, Unit>
+}
