fcf513d0b660903a757a217f9019c45d22f529c6
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-13 14:43:44 +0100

    feat: Registered use cases in DI

--- domain/src/main/java/it/unibolss/smartparking/domain/di/DomainModule.kt
+++ domain/src/main/java/it/unibolss/smartparking/domain/di/DomainModule.kt
@@ -1,8 +1,40 @@
 package it.unibolss.smartparking.domain.di
 
+import it.unibolss.smartparking.domain.usecases.parkingslot.FindParkingSlots
+import it.unibolss.smartparking.domain.usecases.parkingslot.FreeParkingSlot
+import it.unibolss.smartparking.domain.usecases.parkingslot.IncrementParkingSlotOccupation
+import it.unibolss.smartparking.domain.usecases.parkingslot.OccupyParkingSlot
+import it.unibolss.smartparking.domain.usecases.parkingslot.ViewParkingSlot
+import it.unibolss.smartparking.domain.usecases.user.ChangeUserPassword
+import it.unibolss.smartparking.domain.usecases.user.DeleteUser
+import it.unibolss.smartparking.domain.usecases.user.GetUser
+import it.unibolss.smartparking.domain.usecases.user.LoginUser
+import it.unibolss.smartparking.domain.usecases.user.LogoutUser
+import it.unibolss.smartparking.domain.usecases.user.ObserveAuthState
+import it.unibolss.smartparking.domain.usecases.user.SignUpUser
+import it.unibolss.smartparking.domain.usecases.user.ValidateUserEmail
+import it.unibolss.smartparking.domain.usecases.user.ValidateUserName
+import it.unibolss.smartparking.domain.usecases.user.ValidateUserPassword
+
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 internal val useCaseModule = module {
+    singleOf(::FindParkingSlots)
+    singleOf(::FreeParkingSlot)
+    singleOf(::IncrementParkingSlotOccupation)
+    singleOf(::OccupyParkingSlot)
+    singleOf(::ViewParkingSlot)
+    singleOf(::ChangeUserPassword)
+    singleOf(::DeleteUser)
+    singleOf(::GetUser)
+    singleOf(::LoginUser)
+    singleOf(::LogoutUser)
+    singleOf(::ObserveAuthState)
+    singleOf(::SignUpUser)
+    singleOf(::ValidateUserEmail)
+    singleOf(::ValidateUserName)
+    singleOf(::ValidateUserPassword)
 }
 
 val domainModule get() = useCaseModule
