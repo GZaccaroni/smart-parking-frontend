6b3bd4f2a4926ecc38cf9642332ffe1f5e614077
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-13 12:45:54 +0100

    feat: Implemented  change user password use case

---
+++ domain/src/main/java/it/unibolss/smartparking/domain/usecases/user/ChangeUserPassword.kt
@@ -0,0 +1,33 @@
+package it.unibolss.smartparking.domain.usecases.user
+
+import arrow.core.Either
+import it.unibolss.smartparking.domain.entities.common.AppError
+import it.unibolss.smartparking.domain.entities.user.AuthState
+import it.unibolss.smartparking.domain.repositories.user.UserRepository
+import it.unibolss.smartparking.domain.usecases.common.AsyncFailableUseCase
+
+/**
+ * Changes the current password [Params.currentPassword] of the user with the new one
+ * provided [Params.newPassword]. If the user is not logged in it returns an instance of
+ * [Either.Left] containing [AppError.Unauthorized]
+ */
+class ChangeUserPassword(
+    private val validateUserPassword: ValidateUserPassword,
+    private val userRepository: UserRepository
+) : AsyncFailableUseCase<ChangeUserPassword.Params, AppError, Unit>() {
+
+    override suspend fun run(params: Params): Either<AppError, Unit> {
+        if (!validateUserPassword(ValidateUserPassword.Params(params.newPassword)))
+            return Either.Left(AppError.InvalidUserPassword)
+        if (userRepository.authState is AuthState.Guest)
+            return Either.Left(AppError.Unauthorized)
+        return userRepository.changeUserPassword(
+            params.currentPassword,
+            params.newPassword
+        )
+    }
+    data class Params(
+        val currentPassword: String,
+        val newPassword: String
+    )
+}
