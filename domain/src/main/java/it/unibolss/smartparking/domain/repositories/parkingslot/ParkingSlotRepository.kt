e447c7b06faa1e5cbf2eb9e776bb5f28ea563093
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-13 10:03:49 +0100

    chore: Improved ParkingSlotRepository documentation

--- domain/src/main/java/it/unibolss/smartparking/domain/repositories/parkingslot/ParkingSlotRepository.kt
+++ domain/src/main/java/it/unibolss/smartparking/domain/repositories/parkingslot/ParkingSlotRepository.kt
@@ -6,10 +6,28 @@ import it.unibolss.smartparking.domain.entities.geo.GeoPosition
 import it.unibolss.smartparking.domain.entities.parkingslot.ParkingSlot
 
 interface ParkingSlotRepository {
+    /**
+     * Finds all the parking slots in a radius of [radius] meters from [center]
+     */
     suspend fun getParkingSlots(center: GeoPosition, radius: Double): Either<AppError, List<ParkingSlot>>
+
+    /**
+     * Finds a parking slot identified by [id]
+     */
     suspend fun getParkingSlot(id: String): Either<AppError, ParkingSlot?>
 
-    suspend fun occupyParkingSlot(id: String): Either<AppError, List<ParkingSlot>>
-    suspend fun incrementParkingSlotOccupation(id: String): Either<AppError, List<ParkingSlot>>
-    suspend fun freeParkingSlot(id: String): Either<AppError, List<ParkingSlot>>
+    /**
+     * Occupies a parking slot identified by [id]
+     */
+    suspend fun occupyParkingSlot(id: String): Either<AppError, Unit>
+
+    /**
+     * Increments the occupation of a parking slot identified by [id]
+     */
+    suspend fun incrementParkingSlotOccupation(id: String): Either<AppError, Unit>
+
+    /**
+     * Frees the parking slot identified by [id]
+     */
+    suspend fun freeParkingSlot(id: String): Either<AppError, Unit>
 }
