3ab87c553f2bb5fd3b406b2de204eccb0a1339df
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-13 10:04:35 +0100

    refactor: Used sealed interface instead of class for states

--- domain/src/main/java/it/unibolss/smartparking/domain/entities/parkingslot/ParkingSlotState.kt
+++ domain/src/main/java/it/unibolss/smartparking/domain/entities/parkingslot/ParkingSlotState.kt
@@ -2,7 +2,7 @@ package it.unibolss.smartparking.domain.entities.parkingslot
 
 import java.util.Date
 
-sealed class ParkingSlotState {
-    object Free: ParkingSlotState()
-    data class Occupied(val freesAt: Date) : ParkingSlotState()
+sealed interface ParkingSlotState {
+    object Free: ParkingSlotState
+    data class Occupied(val freesAt: Date) : ParkingSlotState
 }
