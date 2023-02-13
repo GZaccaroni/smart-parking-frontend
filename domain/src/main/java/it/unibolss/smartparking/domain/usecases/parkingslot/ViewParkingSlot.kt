beb45d52325c444d69d62e21d59f41cf1b47bfe1
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-13 11:50:51 +0100

    feat: Implemented ViewParkingSlot use case

---
+++ domain/src/main/java/it/unibolss/smartparking/domain/usecases/parkingslot/ViewParkingSlot.kt
@@ -0,0 +1,22 @@
+package it.unibolss.smartparking.domain.usecases.parkingslot
+
+import arrow.core.Either
+import it.unibolss.smartparking.domain.entities.common.AppError
+import it.unibolss.smartparking.domain.entities.parkingslot.ParkingSlot
+import it.unibolss.smartparking.domain.repositories.parkingslot.ParkingSlotRepository
+import it.unibolss.smartparking.domain.usecases.common.AsyncFailableUseCase
+
+/**
+ * Finds the information of a parking slot identified by [Params.id]
+ */
+class ViewParkingSlot(
+    private val parkingSlotRepository: ParkingSlotRepository
+) : AsyncFailableUseCase<ViewParkingSlot.Params, AppError, ParkingSlot?>() {
+
+    override suspend fun run(params: Params): Either<AppError, ParkingSlot?> =
+        parkingSlotRepository.getParkingSlot(params.id)
+
+    data class Params(
+        val id: String,
+    )
+}
---
+++ domain/src/test/java/it/unibolss/smartparking/domain/usecases/parkingslot/ViewParkingSlotTest.kt
@@ -0,0 +1,53 @@
+package it.unibolss.smartparking.domain.usecases.parkingslot
+
+import arrow.core.Either
+import io.mockk.Called
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.verify
+import it.unibolss.smartparking.domain.entities.common.AppError
+import it.unibolss.smartparking.domain.entities.parkingslot.ParkingSlot
+import it.unibolss.smartparking.domain.entities.user.User
+import it.unibolss.smartparking.domain.repositories.parkingslot.ParkingSlotRepository
+import it.unibolss.smartparking.domain.repositories.user.UserRepository
+import it.unibolss.smartparking.domain.usecases.common.invoke
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class ViewParkingSlotTest {
+    @MockK
+    lateinit var parkingSlotRepository: ParkingSlotRepository
+
+    lateinit var useCase: ViewParkingSlot
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        useCase = ViewParkingSlot(parkingSlotRepository)
+    }
+
+    @Test
+    fun testHappyCase() = runTest {
+        val parkingSlotId = "1"
+        val parkingSlot = mockk<ParkingSlot>()
+
+        coEvery {
+            parkingSlotRepository.getParkingSlot(parkingSlotId)
+        } returns Either.Right(parkingSlot)
+
+        val result = useCase(ViewParkingSlot.Params(parkingSlotId))
+        assertEquals(result, Either.Right(parkingSlot))
+
+        coVerify(exactly = 1) {
+            parkingSlotRepository.getParkingSlot(parkingSlotId)
+        }
+    }
+}
