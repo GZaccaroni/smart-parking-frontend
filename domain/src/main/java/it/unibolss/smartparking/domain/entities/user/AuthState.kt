df86adee1a212115937a1c779c85a1c094b13cfb
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-13 10:04:35 +0100

    refactor: Used sealed interface instead of class for states

--- domain/src/main/java/it/unibolss/smartparking/domain/entities/user/AuthState.kt
+++ domain/src/main/java/it/unibolss/smartparking/domain/entities/user/AuthState.kt
@@ -1,6 +1,9 @@
 package it.unibolss.smartparking.domain.entities.user
 
-sealed class AuthState {
-    data class LoggedIn(val id: String): AuthState()
-    object LoggedOut: AuthState()
+/**
+ * The authentication state of the user: [LoggedIn] or [Guest]
+ */
+sealed interface AuthState {
+    data class LoggedIn(val id: String): AuthState
+    object Guest: AuthState
 }
