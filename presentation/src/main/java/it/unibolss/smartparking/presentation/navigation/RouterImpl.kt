6c123ce932a40f1db1b4d2134baca51803d01be0
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-14 13:49:41 +0100

    fix: Avoid exposing MutableStateFlow in RouterImpl

--- presentation/src/main/java/it/unibolss/smartparking/presentation/navigation/RouterImpl.kt
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/navigation/RouterImpl.kt
@@ -2,14 +2,18 @@ package it.unibolss.smartparking.presentation.navigation
 
 import androidx.navigation.NavOptions
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class RouterImpl : Router {
-    override val commands = MutableStateFlow<RouterCommand?>(null)
+    override val commands: StateFlow<RouterCommand?>
+        get() = mutableCommands
+
+    private val mutableCommands = MutableStateFlow<RouterCommand?>(null)
 
     override fun navigateTo(route: Route, navOptions: NavOptions?) {
-        commands.value = RouterCommand.NavigateTo(route, navOptions)
+        mutableCommands.value = RouterCommand.NavigateTo(route, navOptions)
     }
     override fun popBackStack() {
-        commands.value = RouterCommand.PopBackStack
+        mutableCommands.value = RouterCommand.PopBackStack
     }
 }
