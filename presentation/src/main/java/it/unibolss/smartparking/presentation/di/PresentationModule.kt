9e74e5c5ea01c1b228320c893d27189898b8347e
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-14 13:34:52 +0100

    refactor: Extracted Router interface

--- presentation/src/main/java/it/unibolss/smartparking/presentation/di/PresentationModule.kt
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/di/PresentationModule.kt
@@ -1,11 +1,12 @@
 package it.unibolss.smartparking.presentation.di
 
 import it.unibolss.smartparking.presentation.navigation.Router
+import it.unibolss.smartparking.presentation.navigation.RouterImpl
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 internal val navigationModule = module {
-    singleOf(::Router)
+    singleOf<Router>(::RouterImpl)
 }
 internal val viewModelsModule = module {
 }
