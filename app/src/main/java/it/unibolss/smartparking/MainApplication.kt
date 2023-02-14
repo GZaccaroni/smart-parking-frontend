47e2fe25a689dcb710ccaa8f8fdf62b3d11991b9
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-14 12:29:28 +0100

    style: Fixed MainApplication import orders

--- app/src/main/java/it/unibolss/smartparking/MainApplication.kt
+++ app/src/main/java/it/unibolss/smartparking/MainApplication.kt
@@ -1,11 +1,11 @@
 package it.unibolss.smartparking
 
 import android.app.Application
+import it.unibolss.smartparking.data.di.dataModule
+import it.unibolss.smartparking.domain.di.domainModule
+import it.unibolss.smartparking.presentation.di.presentationModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
-import it.unibolss.smartparking.domain.di.domainModule
-import it.unibolss.smartparking.data.di.dataModule
-import it.unibolss.smartparking.presentation.di.presentationModule
 
 class MainApplication : Application() {
     override fun onCreate() {
