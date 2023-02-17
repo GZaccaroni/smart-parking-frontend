f4f11b2faea0a940bb6001e8ef749755a8f8e529
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-17 21:56:23 +0100

    feat: Registered data sources in dependency injection

--- data/src/main/java/it/unibolss/smartparking/data/di/DataModule.kt
+++ data/src/main/java/it/unibolss/smartparking/data/di/DataModule.kt
@@ -1,8 +1,29 @@
 package it.unibolss.smartparking.data.di
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import it.unibolss.smartparking.data.datasources.parkingslot.ParkingSlotDataSource
+import it.unibolss.smartparking.data.datasources.user.UserDataSource
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.create
 
+@OptIn(ExperimentalSerializationApi::class)
 internal val dataSourceModule = module {
+    val mediaType = MediaType.parse("application/json")!!
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://localhost/")
+        .addConverterFactory(Json.asConverterFactory(mediaType))
+        .build()
+
+    single<ParkingSlotDataSource> {
+        retrofit.create()
+    }
+    single<UserDataSource> {
+        retrofit.create()
+    }
 }
 
 internal val repositoryModule = module {
