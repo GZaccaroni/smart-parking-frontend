eb57704186cd4a398b667faf22afac6510fa1998
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-17 22:19:39 +0100

    feat: Registered call methods in retrofit

--- data/src/main/java/it/unibolss/smartparking/data/datasources/user/UserDataSource.kt
+++ data/src/main/java/it/unibolss/smartparking/data/datasources/user/UserDataSource.kt
@@ -5,21 +5,43 @@ import it.unibolss.smartparking.data.models.user.LoginRequestBody
 import it.unibolss.smartparking.data.models.user.SignUpRequestBody
 import it.unibolss.smartparking.data.models.user.UserDto
 import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 internal interface UserDataSource {
 
+    @POST("user/sign-up")
     suspend fun signUp(
-        user: SignUpRequestBody
-    ): Response<Unit>
+        @Body
+        body: SignUpRequestBody
+    ): Response<UserDto>
 
-    suspend fun login(credentials: LoginRequestBody): Response<Unit>
+    @POST("user/login")
+    suspend fun login(
+        @Body
+        body: LoginRequestBody
+    ): Response<UserDto>
 
+    @POST("user/logout")
     suspend fun logout(): Response<Unit>
 
+    @POST("user/{id}/change-password")
     suspend fun changeUserPassword(
         body: ChangePasswordRequestBody
     ): Response<Unit>
 
-    suspend fun getUser(): Response<UserDto>
-    suspend fun deleteUser(): Response<Unit>
+    @GET("user/{id}")
+    suspend fun getUser(
+        @Path("id")
+        id: String,
+    ): Response<UserDto>
+
+    @DELETE("user/{id}")
+    suspend fun deleteUser(
+        @Path("id")
+        id: String,
+    ): Response<Unit>
 }
