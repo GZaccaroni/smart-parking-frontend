e6e8eb1ee79019f0e3d107b16d136185ff5f4f86
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-20 09:02:05 +0100

    test: Implemented tests for Route

--- presentation/src/main/java/it/unibolss/smartparking/presentation/navigation/RouterImpl.kt
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/navigation/RouterImpl.kt
@@ -3,17 +3,16 @@ package it.unibolss.smartparking.presentation.navigation
 import androidx.navigation.NavOptions
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class RouterImpl : Router {
-    override val commands: StateFlow<RouterCommand?>
-        get() = mutableCommands
-
-    private val mutableCommands = MutableStateFlow<RouterCommand?>(null)
+    private val _commands = MutableStateFlow<RouterCommand?>(null)
+    override val commands: StateFlow<RouterCommand?> = _commands.asStateFlow()
 
     override fun navigateTo(route: Route, navOptions: NavOptions?) {
-        mutableCommands.value = RouterCommand.NavigateTo(route, navOptions)
+        _commands.value = RouterCommand.NavigateTo(route, navOptions)
     }
     override fun popBackStack() {
-        mutableCommands.value = RouterCommand.PopBackStack
+        _commands.value = RouterCommand.PopBackStack
     }
 }
