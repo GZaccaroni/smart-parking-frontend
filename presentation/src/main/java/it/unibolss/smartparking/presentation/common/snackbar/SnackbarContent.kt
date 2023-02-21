eaf3e54ab8d7aa5dc8d9df6fcb9c97a862c09f3f
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-21 09:53:34 +0100

    feat: show content in snackbar

--- presentation/src/main/java/it/unibolss/smartparking/presentation/common/snackbar/SnackbarContent.kt
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/common/snackbar/SnackbarContent.kt
@@ -1,5 +1,25 @@
 package it.unibolss.smartparking.presentation.common.snackbar
 
-data class SnackbarContent(
+import androidx.compose.runtime.Composable
+import it.unibolss.smartparking.domain.entities.common.AppError
+import it.unibolss.smartparking.presentation.common.error.message
+import java.util.UUID
+
+sealed interface SnackbarContent {
+    val id: String
+
+    @get:Composable
     val message: String
-)
+
+    class Error(private val error: AppError) : SnackbarContent {
+        override val id: String
+            get() = UUID.randomUUID().toString()
+
+        override val message: String
+            @Composable
+            get() = error.message
+
+        override fun equals(other: Any?): Boolean =
+            if (other is Error) (other.id == id) else false
+    }
+}
