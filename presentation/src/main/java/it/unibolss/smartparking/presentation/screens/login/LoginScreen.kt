8dfde7334904c494bc79e8bd7e33980fd75fc4e4
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-22 21:16:33 +0100

    chore: removed unused import

--- presentation/src/main/java/it/unibolss/smartparking/presentation/screens/login/LoginScreen.kt
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/screens/login/LoginScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import it.unibolss.smartparking.presentation.R
