01e186a30d92be1fc5d69144ead7a27b4fc49ac8
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-23 20:48:06 +0100

    feat: sign up screen view model

---
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/screens/signup/SignUpScreenViewModel.kt
@@ -0,0 +1,107 @@
+package it.unibolss.smartparking.presentation.screens.signup
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavOptions
+import it.unibolss.smartparking.domain.usecases.user.SignUpUser
+import it.unibolss.smartparking.domain.usecases.user.ValidateUserEmail
+import it.unibolss.smartparking.domain.usecases.user.ValidateUserName
+import it.unibolss.smartparking.domain.usecases.user.ValidateUserPassword
+import it.unibolss.smartparking.presentation.common.snackbar.SnackbarContent
+import it.unibolss.smartparking.presentation.navigation.Router
+import it.unibolss.smartparking.presentation.screens.parkingslots.ParkingSlotsRoute
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+internal class SignUpScreenViewModel(
+    private val signUpUser: SignUpUser,
+    private val validateUserName: ValidateUserName,
+    private val validateUserEmail: ValidateUserEmail,
+    private val validateUserPassword: ValidateUserPassword,
+    private val router: Router,
+) : ViewModel() {
+
+    private val _snackbar = MutableSharedFlow<SnackbarContent>(0)
+    val snackbar: SharedFlow<SnackbarContent> = _snackbar.asSharedFlow()
+
+    private val _name = MutableStateFlow("")
+    val name: StateFlow<String> = _name.asStateFlow()
+
+    private val _email = MutableStateFlow("")
+    val email: StateFlow<String> = _email.asStateFlow()
+
+    private val _password = MutableStateFlow("")
+    val password: StateFlow<String> = _password.asStateFlow()
+
+    val nameError: StateFlow<Boolean> =
+        _name
+            .drop(1)
+            .map { !validateUserName(ValidateUserName.Params(it)) }
+            .stateIn(viewModelScope, SharingStarted.Eagerly, false)
+
+    val emailError: StateFlow<Boolean> =
+        _email
+            .drop(1)
+            .map { !validateUserEmail(ValidateUserEmail.Params(it)) }
+            .stateIn(viewModelScope, SharingStarted.Eagerly, false)
+
+    val passwordError: StateFlow<Boolean> =
+        _password
+            .drop(1)
+            .map { !validateUserPassword(ValidateUserPassword.Params(it)) }
+            .stateIn(viewModelScope, SharingStarted.Eagerly, false)
+
+    val submitButtonEnabled: StateFlow<Boolean> =
+        combine(email, password) { email, password ->
+            val emailValid = validateUserEmail(ValidateUserEmail.Params(email))
+            val passwordValid = validateUserPassword(ValidateUserPassword.Params(password))
+            emailValid && passwordValid
+        }.stateIn(viewModelScope, SharingStarted.Eagerly, false)
+
+    private val _loading = MutableStateFlow(false)
+    val loading: StateFlow<Boolean> = _loading.asStateFlow()
+
+    fun setName(name: String) {
+        _name.value = name.trim()
+    }
+    fun setEmail(email: String) {
+        _email.value = email.trim()
+    }
+    fun setPassword(password: String) {
+        _password.value = password
+    }
+
+    fun submit() {
+        viewModelScope.launch {
+            _loading.value = true
+            val result = signUpUser(
+                SignUpUser.Params(
+                    name = name.value,
+                    email = email.value,
+                    password = password.value
+                )
+            )
+
+            _loading.value = false
+            result.fold(
+                { _snackbar.emit(SnackbarContent.Error(it)) },
+                {
+                    router.navigateTo(
+                        ParkingSlotsRoute,
+                        NavOptions.Builder().setPopUpTo(0, true).build(),
+                    )
+                }
+            )
+        }
+    }
+}
