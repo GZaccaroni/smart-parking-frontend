e3127415ab409725a4342dfd73ba6302f12d6e21
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-23 21:18:42 +0100

    feat: registered sign up screen view model in DI

--- presentation/src/main/java/it/unibolss/smartparking/presentation/di/PresentationModule.kt
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/di/PresentationModule.kt
@@ -3,6 +3,7 @@ package it.unibolss.smartparking.presentation.di
 import it.unibolss.smartparking.presentation.navigation.Router
 import it.unibolss.smartparking.presentation.navigation.RouterImpl
 import it.unibolss.smartparking.presentation.screens.login.LoginScreenViewModel
+import it.unibolss.smartparking.presentation.screens.signup.SignUpScreenViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -12,6 +13,7 @@ internal val navigationModule = module {
 }
 internal val viewModelsModule = module {
     viewModelOf(::LoginScreenViewModel)
+    viewModelOf(::SignUpScreenViewModel)
 }
 
 val presentationModule get() = navigationModule + viewModelsModule
