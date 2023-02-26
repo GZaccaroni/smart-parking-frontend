05935379b5065ed6efb56cb2b43c58513429190a
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-26 18:08:02 +0100

    test: added turbine

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -67,6 +67,7 @@ object Deps {
             const val junitAndroidExt = "1.1.5"
             const val espressoCore = "3.5.1"
             const val mockk = "1.13.4"
+            const val turbine = "0.12.1"
         }
         const val junit = "junit:junit:${Versions.junit}"
         const val junitAndroidExt = "androidx.test.ext:junit-ktx:${Versions.junitAndroidExt}"
@@ -74,6 +75,7 @@ object Deps {
         const val uiTestJunit4 = "androidx.compose.ui:ui-test-junit4:${Libraries.Versions.androidXCompose}"
         const val kotlinCoroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Libraries.Versions.kotlinCoroutines}"
         const val mockk = "io.mockk:mockk:${Versions.mockk}"
+        const val turbine = "app.cash.turbine:turbine:${Versions.turbine}"
     }
 
     object DebugLibraries {
