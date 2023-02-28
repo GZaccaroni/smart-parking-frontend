95fc174d6bc59f6e7573ece1aa824cf425b1ca3c
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-28 17:10:52 +0100

    feat: added stop end to occupy and increment occupation

--- domain/src/main/java/it/unibolss/smartparking/domain/usecases/parkingslot/ViewParkingSlot.kt
+++ domain/src/main/java/it/unibolss/smartparking/domain/usecases/parkingslot/ViewParkingSlot.kt
@@ -5,15 +5,16 @@ import it.unibolss.smartparking.domain.entities.common.AppError
 import it.unibolss.smartparking.domain.entities.parkingslot.ParkingSlot
 import it.unibolss.smartparking.domain.repositories.parkingslot.ParkingSlotRepository
 import it.unibolss.smartparking.domain.usecases.common.AsyncFailableUseCase
+import it.unibolss.smartparking.domain.usecases.parkingslot.ViewParkingSlot.Params
 
 /**
  * Finds the information of a parking slot identified by [Params.id]
  */
 class ViewParkingSlot(
     private val parkingSlotRepository: ParkingSlotRepository
-) : AsyncFailableUseCase<ViewParkingSlot.Params, AppError, ParkingSlot?>() {
+) : AsyncFailableUseCase<ViewParkingSlot.Params, AppError, ParkingSlot>() {
 
-    override suspend fun run(params: Params): Either<AppError, ParkingSlot?> =
+    override suspend fun run(params: Params): Either<AppError, ParkingSlot> =
         parkingSlotRepository.getParkingSlot(params.id)
 
     data class Params(
