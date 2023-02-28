f7c8b62baeb37154571e25dd4283efa68496a430
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-28 17:10:17 +0100

    feat: check if stop end is valid

--- domain/src/main/java/it/unibolss/smartparking/domain/usecases/parkingslot/IncrementParkingSlotOccupation.kt
+++ domain/src/main/java/it/unibolss/smartparking/domain/usecases/parkingslot/IncrementParkingSlotOccupation.kt
@@ -3,8 +3,11 @@ package it.unibolss.smartparking.domain.usecases.parkingslot
 import arrow.core.Either
 import arrow.core.flatMap
 import it.unibolss.smartparking.domain.entities.common.AppError
+import it.unibolss.smartparking.domain.entities.parkingslot.ParkingSlotState
 import it.unibolss.smartparking.domain.repositories.parkingslot.ParkingSlotRepository
 import it.unibolss.smartparking.domain.usecases.common.AsyncFailableUseCase
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 
 /**
  * Increments the parking slot currently occupied by the user. If the user isn't parking it returns
@@ -12,15 +15,21 @@ import it.unibolss.smartparking.domain.usecases.common.AsyncFailableUseCase
  */
 class IncrementParkingSlotOccupation(
     private val parkingSlotRepository: ParkingSlotRepository
-) : AsyncFailableUseCase<Unit, AppError, Unit>() {
+) : AsyncFailableUseCase<IncrementParkingSlotOccupation.Params, AppError, Unit>() {
 
-    override suspend fun run(params: Unit): Either<AppError, Unit> =
+    override suspend fun run(params: Params): Either<AppError, Unit> =
         parkingSlotRepository.getCurrentParkingSlot()
             .flatMap {
-                if (it == null) {
-                    Either.Left(AppError.NoParkingSlotOccupied)
-                } else {
-                    parkingSlotRepository.incrementParkingSlotOccupation(it.id)
+                if (it == null || it.state !is ParkingSlotState.Occupied) {
+                    return@flatMap Either.Left(AppError.NoParkingSlotOccupied)
                 }
+                if (params.stopEnd <= it.state.freesAt || params.stopEnd <= Clock.System.now()) {
+                    return@flatMap Either.Left(AppError.InvalidStopEnd)
+                }
+                parkingSlotRepository.incrementParkingSlotOccupation(it.id, params.stopEnd)
             }
+
+    data class Params(
+        val stopEnd: Instant
+    )
 }
