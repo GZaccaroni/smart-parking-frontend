0ad04ac4c0acccb0862e1ec44a7ba10ca7179cd6
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-28 17:11:29 +0100

    build!: incremented android min sdk version

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,7 +2,7 @@ object AppConfig {
     const val applicationId = "it.unibolss.smartparking";
 
     object AndroidSdk {
-        const val min = 24
+        const val min = 26
         const val compile = 33
         const val target = compile
     }
