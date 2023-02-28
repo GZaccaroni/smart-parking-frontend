57eec5724e84ce2e4875a1468681d2b1760ecc3e
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-28 17:08:05 +0100

    feat: added format date method for kotlinx instant

---
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/common/date/FormatDate.kt
@@ -0,0 +1,17 @@
+package it.unibolss.smartparking.presentation.common.date
+
+import kotlinx.datetime.Instant
+import kotlinx.datetime.toJavaInstant
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+
+fun Instant.formatted(
+    formatStyle: FormatStyle
+): String {
+    val javaInstant = this.toJavaInstant()
+    val zonedDateTime = ZonedDateTime.ofInstant(javaInstant, ZoneId.systemDefault())
+    val formatter = DateTimeFormatter.ofLocalizedDateTime(formatStyle)
+    return formatter.format(zonedDateTime)
+}
