911295e0c0f3d4c8f1362a107d8a2475c10d1367
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-02-28 17:09:45 +0100

    feat: registered parking slot route

--- presentation/src/main/java/it/unibolss/smartparking/presentation/screens/parkingslot/ParkingSlotNavigation.kt
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/screens/parkingslot/ParkingSlotNavigation.kt
@@ -1,9 +1,9 @@
 package it.unibolss.smartparking.presentation.screens.parkingslot
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import it.unibolss.smartparking.presentation.navigation.Route
 
 data class ParkingSlotRoute(val id: String) : Route {
@@ -11,9 +11,11 @@ data class ParkingSlotRoute(val id: String) : Route {
 }
 
 fun NavGraphBuilder.parkingSlotScreen() {
-    composable("parking-slots/{id}") {
-        Box(
-            modifier = Modifier
-        )
+    composable(
+        "parking-slots/{id}",
+        arguments = listOf(navArgument("id") { type = NavType.StringType })
+    ) { backStackEntry ->
+        val parkingSlotId = backStackEntry.arguments?.getString("id")!!
+        ParkingSlotScreen(parkingSlotId = parkingSlotId)
     }
 }
