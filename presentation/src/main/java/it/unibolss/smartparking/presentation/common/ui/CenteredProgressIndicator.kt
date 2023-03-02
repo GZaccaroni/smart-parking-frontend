d5cbc2eb4cafc802d986ab0c5d609d0c8b1533fa
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-02 15:17:30 +0100

    feat: centered progress indicator

---
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/common/ui/CenteredProgressIndicator.kt
@@ -0,0 +1,20 @@
+package it.unibolss.smartparking.presentation.common.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CenteredProgressIndicator() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator(modifier = Modifier.size(32.dp))
+    }
+}
