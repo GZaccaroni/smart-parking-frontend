0c5189ac1a846ea68bcb55eab7841eb2584fbb6c
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-02 15:19:46 +0100

    fix: view current parking slot DI registration

--- domain/src/main/java/it/unibolss/smartparking/domain/di/DomainModule.kt
+++ domain/src/main/java/it/unibolss/smartparking/domain/di/DomainModule.kt
@@ -4,23 +4,24 @@ import it.unibolss.smartparking.domain.usecases.parkingslot.FindParkingSlots
 import it.unibolss.smartparking.domain.usecases.parkingslot.FreeParkingSlot
 import it.unibolss.smartparking.domain.usecases.parkingslot.IncrementParkingSlotOccupation
 import it.unibolss.smartparking.domain.usecases.parkingslot.OccupyParkingSlot
+import it.unibolss.smartparking.domain.usecases.parkingslot.ViewCurrentParkingSlot
 import it.unibolss.smartparking.domain.usecases.parkingslot.ViewParkingSlot
 import it.unibolss.smartparking.domain.usecases.user.ChangeUserPassword
 import it.unibolss.smartparking.domain.usecases.user.DeleteUser
+import it.unibolss.smartparking.domain.usecases.user.GetAuthState
 import it.unibolss.smartparking.domain.usecases.user.GetUser
 import it.unibolss.smartparking.domain.usecases.user.LoginUser
 import it.unibolss.smartparking.domain.usecases.user.LogoutUser
-import it.unibolss.smartparking.domain.usecases.user.GetAuthState
 import it.unibolss.smartparking.domain.usecases.user.SignUpUser
 import it.unibolss.smartparking.domain.usecases.user.ValidateUserEmail
 import it.unibolss.smartparking.domain.usecases.user.ValidateUserName
 import it.unibolss.smartparking.domain.usecases.user.ValidateUserPassword
-
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 internal val useCaseModule = module {
     singleOf(::FindParkingSlots)
+    singleOf(::ViewCurrentParkingSlot)
     singleOf(::FreeParkingSlot)
     singleOf(::IncrementParkingSlotOccupation)
     singleOf(::OccupyParkingSlot)
