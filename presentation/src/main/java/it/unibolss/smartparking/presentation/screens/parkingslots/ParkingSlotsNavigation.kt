999b19ee5807636bfef3db88ea0b7b8673112007
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-02 15:18:11 +0100

    feat: parking slots screen view model

--- presentation/src/main/java/it/unibolss/smartparking/presentation/screens/parkingslots/ParkingSlotsNavigation.kt
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/screens/parkingslots/ParkingSlotsNavigation.kt
@@ -1,7 +1,5 @@
 package it.unibolss.smartparking.presentation.screens.parkingslots
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import it.unibolss.smartparking.presentation.navigation.Route
@@ -12,8 +10,6 @@ object ParkingSlotsRoute : Route {
 
 fun NavGraphBuilder.parkingSlotsScreen() {
     composable("parking-slots") {
-        Box(
-            modifier = Modifier
-        )
+        ParkingSlotsScreen()
     }
 }
