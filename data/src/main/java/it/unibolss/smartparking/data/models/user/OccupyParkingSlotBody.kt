7997fb236d3326d0fbff77e4e72e2f33097926fc
cataniandrea <91258294+acataniUNIBO@users.noreply.github.com>
2023-03-05 11:43:29 +0100

    Update OccupyParkingSlotBody.kt

--- data/src/main/java/it/unibolss/smartparking/data/models/user/OccupyParkingSlotBody.kt
+++ data/src/main/java/it/unibolss/smartparking/data/models/user/OccupyParkingSlotBody.kt
@@ -1,7 +1,10 @@
 package it.unibolss.smartparking.data.models.user
 
 import kotlinx.datetime.Instant
+import kotlinx.serialization.Serializable
 
+/*
 internal data class OccupyParkingSlotBody(
     val stopEnd: Instant
 )
+*/
