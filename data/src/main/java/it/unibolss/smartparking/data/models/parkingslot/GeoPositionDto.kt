b8c2e56842eef6e94c76e5a83671b88c7339a745
cataniandrea <91258294+acataniUNIBO@users.noreply.github.com>
2023-03-05 11:40:32 +0100

    Update GeoPositionDto.kt

--- data/src/main/java/it/unibolss/smartparking/data/models/parkingslot/GeoPositionDto.kt
+++ data/src/main/java/it/unibolss/smartparking/data/models/parkingslot/GeoPositionDto.kt
@@ -2,14 +2,16 @@ package it.unibolss.smartparking.data.models.parkingslot
 
 import kotlinx.serialization.Serializable
 
-/*@Serializable
+@Serializable
 internal data class GeoPositionDto(
     val longitude: Double,
     val latitude: Double
-)*/
+)
 
+/*
 @Serializable
 internal data class Position(
     val longitude: Double,
     val latitude: Double
 )
+*/
