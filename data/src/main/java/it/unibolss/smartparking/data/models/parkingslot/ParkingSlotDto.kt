ebf9caf06eb7e42037a49cad7605ad8276eb48ff
cataniandrea <91258294+acataniUNIBO@users.noreply.github.com>
2023-03-05 11:35:07 +0100

    Update ParkingSlotDto.kt

--- data/src/main/java/it/unibolss/smartparking/data/models/parkingslot/ParkingSlotDto.kt
+++ data/src/main/java/it/unibolss/smartparking/data/models/parkingslot/ParkingSlotDto.kt
@@ -3,10 +3,22 @@ package it.unibolss.smartparking.data.models.parkingslot
 import kotlinx.datetime.Instant
 import kotlinx.serialization.Serializable
 
+/*
 @Serializable
 internal data class ParkingSlotDto(
     val id: String,
     val position: GeoPositionDto,
     val occupied: Boolean,
     val stopEnd: Instant?
+)
+*/
+
+@Serializable
+internal data class ParkingSlotDto(
+    val id: String,
+    val occupied: Boolean,
+    val stopEnd: String,
+    val latitude: Double,
+    val longitude: Double,
+    val userId: String
 )
