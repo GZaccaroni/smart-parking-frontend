c94941afc688f7a8f281fd77e86f07572e1cd488
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-13 16:43:37 +0100

    docs: change password ui

--- presentation/src/main/java/it/unibolss/smartparking/presentation/screens/user/changepassword/ChangePasswordNavigation.kt
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/screens/user/changepassword/ChangePasswordNavigation.kt
@@ -4,11 +4,17 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import it.unibolss.smartparking.presentation.navigation.Route
 
-internal object ChangePasswordRoute : Route {
+/**
+ * The route to the change password screen
+ */
+object ChangePasswordRoute : Route {
     override val path = "change-password"
 }
 
-internal fun NavGraphBuilder.changePasswordScreen() {
+/**
+ * Utility method to provide the route to change password screen in a [NavGraphBuilder]
+ */
+fun NavGraphBuilder.changePasswordScreen() {
     composable("change-password") {
         ChangePasswordScreen()
     }
