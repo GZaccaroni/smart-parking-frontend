40f375f2406c39ea8dafd63138eac749874ce4e7
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-13 16:50:51 +0100

    docs: user entities

--- domain/src/main/java/it/unibolss/smartparking/domain/entities/user/AuthState.kt
+++ domain/src/main/java/it/unibolss/smartparking/domain/entities/user/AuthState.kt
@@ -4,6 +4,6 @@ package it.unibolss.smartparking.domain.entities.user
  * The authentication state of the user: [LoggedIn] or [Guest]
  */
 sealed interface AuthState {
-    data class LoggedIn(val id: String): AuthState
-    object Guest: AuthState
+    object LoggedIn : AuthState
+    object Guest : AuthState
 }
