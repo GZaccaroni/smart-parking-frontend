5d04ee4b317ba8edb4f05d6a4beb9a435452bdef
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-14 10:28:50 +0100

    docs: geo position

--- domain/src/main/java/it/unibolss/smartparking/domain/entities/geo/GeoPosition.kt
+++ domain/src/main/java/it/unibolss/smartparking/domain/entities/geo/GeoPosition.kt
@@ -1,5 +1,10 @@
 package it.unibolss.smartparking.domain.entities.geo
 
+/**
+ * A geographical position
+ * @param latitude the latitude of the point
+ * @param longitude the longitude of the point
+ */
 data class GeoPosition(
     val latitude: Double,
     val longitude: Double
