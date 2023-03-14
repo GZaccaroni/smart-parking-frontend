b8ab642842dec28929c142c8a325bb1df67503a0
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-14 11:07:27 +0100

    feat: authentication user dtos

--- data/src/main/java/it/unibolss/smartparking/data/models/user/IncrementParkingSlotOccupationBody.kt
+++ data/src/main/java/it/unibolss/smartparking/data/models/user/IncrementParkingSlotOccupationBody.kt
@@ -1,7 +1,9 @@
 package it.unibolss.smartparking.data.models.user
 
 import kotlinx.datetime.Instant
+import kotlinx.serialization.Serializable
 
+@Serializable
 internal data class IncrementParkingSlotOccupationBody(
     val stopEnd: Instant
 )
