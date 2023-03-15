e47a7dbce117036c3e6627dac33274856c8ecd5d
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-15 16:36:24 +0100

    feat(parkingslots): repository get parking slots given a center and a radius

---
+++ data/src/main/java/it/unibolss/smartparking/data/repositories/parkingslot/ParkingSlotRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package it.unibolss.smartparking.data.repositories.parkingslot
+
+import arrow.core.Either
+import it.unibolss.smartparking.data.datasources.parkingslot.ParkingSlotDataSource
+import it.unibolss.smartparking.data.datasources.user.AuthenticationDataSource
+import it.unibolss.smartparking.data.models.parkingslot.GeoPositionDto
+import it.unibolss.smartparking.data.models.parkingslot.ParkingSlotDto
+import it.unibolss.smartparking.data.models.user.IncrementParkingSlotOccupationBody
+import it.unibolss.smartparking.data.models.user.OccupyParkingSlotBody
+import it.unibolss.smartparking.data.repositories.apiCall
+import it.unibolss.smartparking.domain.entities.common.AppError
+import it.unibolss.smartparking.domain.entities.geo.GeoPosition
+import it.unibolss.smartparking.domain.entities.parkingslot.ParkingSlot
+import it.unibolss.smartparking.domain.entities.parkingslot.ParkingSlotState
+import it.unibolss.smartparking.domain.repositories.parkingslot.ParkingSlotRepository
+import kotlinx.datetime.Instant
+
+internal class ParkingSlotRepositoryImpl(
+    private val authenticationDataSource: AuthenticationDataSource,
+    private val parkingSlotDataSource: ParkingSlotDataSource
+) : ParkingSlotRepository {
+
+    override suspend fun getParkingSlots(
+        center: GeoPosition,
+        radius: Double
+    ): Either<AppError, List<ParkingSlot>> {
+        val authInfo = authenticationDataSource.getCurrentAuthInfo()
+            ?: return Either.Left(AppError.Unauthorized)
+        return apiCall {
+            parkingSlotDataSource
+                .getParkingSlots(
+                    latitude = center.latitude,
+                    longitude = center.longitude,
+                    radius = radius
+                )
+        }.map { list ->
+            list.map { it.toDomain(authInfo.userId == it.occupierId) }
+        }
+    }
