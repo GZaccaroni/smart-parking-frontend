857297fde13d638c1c56c390e421bab05ba4819d
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-16 20:19:22 +0100

    test(parkingslot): repository method get parking slots

--- data/src/test/java/it/unibolss/smartparking/data/repositories/parkingslot/ParkingSlotRepositoryImplTest.kt
+++ data/src/test/java/it/unibolss/smartparking/data/repositories/parkingslot/ParkingSlotRepositoryImplTest.kt
@@ -59,6 +59,66 @@ class ParkingSlotRepositoryImplTest {
         )
     }
 
+    @Test
+    fun getParkingSlots() = runTest {
+        val parkingSlotId1 = "testId1"
+        val parkingSlotId2 = "testId2"
+        val parkingSlotId3 = "testId3"
+        val parkingSlotDto1 = sampleParkingSlotDto(parkingSlotId1, currentUserId)
+        val parkingSlotDto2 = sampleParkingSlotDto(parkingSlotId2, "abc")
+        val parkingSlotDto3 = sampleParkingSlotDto(parkingSlotId3, null)
+
+        val parkingSlotsDto = listOf(
+            parkingSlotDto1,
+            parkingSlotDto2,
+            parkingSlotDto3
+        )
+        val position = GeoPosition(0.0, 0.0)
+        val radius = 50.0
+        coEvery {
+            parkingSlotDataSource.getParkingSlots(
+                latitude = position.latitude,
+                longitude = position.longitude,
+                radius = radius,
+            )
+        } returns parkingSlotsDto
+
+        val result = parkingSlotRepository.getParkingSlots(position, radius)
+
+        assertIs<Either.Right<List<ParkingSlot>>>(result)
+        assertEquals(result.value.size, 3)
+
+        assertEntityCorrect(
+            dto = parkingSlotDto1,
+            entity = result.value[0]
+        )
+        assertEntityCorrect(
+            dto = parkingSlotDto2,
+            entity = result.value[1]
+        )
+        assertEntityCorrect(
+            dto = parkingSlotDto3,
+            entity = result.value[2]
+        )
+    }
+
+    @Test
+    fun getParkingSlotsFailure() = runTest {
+        val position = GeoPosition(0.0, 0.0)
+        val radius = 50.0
+        coEvery {
+            parkingSlotDataSource.getParkingSlots(
+                latitude = position.latitude,
+                longitude = position.longitude,
+                radius = radius,
+            )
+        } throws sampleHTTPException(AppErrorDto.Unauthorized)
+
+        val result = parkingSlotRepository.getParkingSlots(position, radius)
+
+        assertEquals(Either.Left(AppError.Unauthorized), result)
+    }
+
     private fun sampleHTTPException(errorCode: AppErrorDto): HttpException {
         val sampleResponse =
             """
