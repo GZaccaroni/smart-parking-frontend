4dcb3b577d016d15256d68240c32f0b8ed73bd2a
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-16 22:26:50 +0100

    test: evaluate app module coverage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
             )
         }
     }
+    buildTypes.all {
+        enableUnitTestCoverage = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
