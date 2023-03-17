625c33e30d97455201eb7f4a469d50026626aca4
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-17 19:45:59 +0100

    docs: documented router

--- presentation/src/main/java/it/unibolss/smartparking/presentation/navigation/RouterCommand.kt
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/navigation/RouterCommand.kt
@@ -2,10 +2,22 @@ package it.unibolss.smartparking.presentation.navigation
 
 import androidx.navigation.NavOptions
 
+/**
+ * This interface represents a command that can be given to a [Router] instance
+ */
 sealed interface RouterCommand {
+
+    /**
+     * This class represents the requests to a [Router] instance to navigate to a specific [route]
+     * with some [navOptions]
+     */
     data class NavigateTo(
         val route: Route,
         val navOptions: NavOptions? = null,
     ) : RouterCommand
+
+    /**
+     * This object represents the requests to a [Router] instance to pop the current screen
+     */
     object PopBackStack : RouterCommand
 }
