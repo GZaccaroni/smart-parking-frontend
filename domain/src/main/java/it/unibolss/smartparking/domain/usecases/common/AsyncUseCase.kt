a2c41cb086f011b613a37838d4238077471a2eb5
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-17 19:44:42 +0100

    docs: use case

--- domain/src/main/java/it/unibolss/smartparking/domain/usecases/common/AsyncUseCase.kt
+++ domain/src/main/java/it/unibolss/smartparking/domain/usecases/common/AsyncUseCase.kt
@@ -1,5 +1,8 @@
 package it.unibolss.smartparking.domain.usecases.common
 
+/**
+ * An abstract base class for defining asynchronous use cases.
+*/
 abstract class AsyncUseCase<in Params, out Result> {
 
     protected abstract suspend fun run(params: Params): Result
