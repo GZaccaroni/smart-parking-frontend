0b1b141e4ada13af2c85e0d1a3697fe8d2d1ad64
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-17 19:45:06 +0100

    docs: documented app alert

--- presentation/src/main/java/it/unibolss/smartparking/presentation/common/appalert/AppAlertState.kt
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/common/appalert/AppAlertState.kt
@@ -1,5 +1,8 @@
 package it.unibolss.smartparking.presentation.common.appalert
 
+/**
+ * Whether an app alert is currently shown or not.
+ */
 sealed interface AppAlertState {
     data class Some(val alert: AppAlert) : AppAlertState
     object None : AppAlertState
--- presentation/src/main/java/it/unibolss/smartparking/presentation/common/appalert/AppAlertType.kt
+++ presentation/src/main/java/it/unibolss/smartparking/presentation/common/appalert/AppAlertType.kt
@@ -1,5 +1,8 @@
 package it.unibolss.smartparking.presentation.common.appalert
 
+/**
+ * The type of an [AppAlert]: it defines how an app alert should be shown.
+ */
 enum class AppAlertType {
     TOAST,
     SNACKBAR;
