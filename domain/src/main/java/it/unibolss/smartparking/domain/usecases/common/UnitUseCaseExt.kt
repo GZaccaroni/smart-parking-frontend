73411588a2f45f7dcf9c1cc18405dda25eec52c1
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-17 19:44:42 +0100

    docs: use case

--- domain/src/main/java/it/unibolss/smartparking/domain/usecases/common/UnitUseCaseExt.kt
+++ domain/src/main/java/it/unibolss/smartparking/domain/usecases/common/UnitUseCaseExt.kt
@@ -1,4 +1,11 @@
 package it.unibolss.smartparking.domain.usecases.common
 
+/**
+ * Utility method to avoid passing [Unit] argument to invoke
+ */
 operator fun <Res> UseCase<Unit, Res>.invoke(): Res = invoke(Unit)
+
+/**
+ * Utility method to avoid passing [Unit] argument to invoke
+ */
 suspend operator fun <Res> AsyncUseCase<Unit, Res>.invoke(): Res = invoke(Unit)
--- domain/src/main/java/it/unibolss/smartparking/domain/usecases/common/UseCase.kt
+++ domain/src/main/java/it/unibolss/smartparking/domain/usecases/common/UseCase.kt
@@ -1,5 +1,8 @@
 package it.unibolss.smartparking.domain.usecases.common
 
+/**
+ * An abstract base class for defining non-asynchronous use cases.
+*/
 abstract class UseCase<in Params, out Result> {
 
     protected abstract fun run(params: Params): Result
