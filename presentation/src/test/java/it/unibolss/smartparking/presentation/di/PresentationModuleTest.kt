29b671f0e4923b4dc89413751fc6ead5e3c439e0
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-04-29 14:31:24 +0200

    refactor: replaced deprecatd koin method

--- presentation/src/test/java/it/unibolss/smartparking/presentation/di/PresentationModuleTest.kt
+++ presentation/src/test/java/it/unibolss/smartparking/presentation/di/PresentationModuleTest.kt
@@ -14,6 +14,7 @@ import org.koin.test.check.checkModules
 class PresentationModuleTest {
     @get:Rule
     val mockkRule = MockKRule(this)
+
     @get:Rule
     val mainDispatcherRule = MainDispatcherRule()
 
